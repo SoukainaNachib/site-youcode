cf08418a668c507d15cdcc66860bd1f8ce2f7c0f
SoukainaNachib <44495672+SoukainaNachib@users.noreply.github.com>
2019-04-15 11:54:38 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function myOverFunction() {
+   document.getElementById("ad").style.display = "block";
+   }
+function myOutFunction() {
+    document.getElementById("ad").style.display = "none";
+}
+function myOverFunction2() {
+   document.getElementById("ad2").style.display = "block";
+   }
+function myOutFunction2() {
+    document.getElementById("ad2").style.display = "none";
+}
+function Anne1() {
+   document.getElementById("anne1").style.display = "block";
+   document.getElementById("anne1").style.marginBottom = "64px";
+   document.getElementById("anne2").style.display = "none";
+   }
+function Anne2() {
+   document.getElementById("anne2").style.display = "block";
+   document.getElementById("anne1").style.display = "none";
+}
+
+
